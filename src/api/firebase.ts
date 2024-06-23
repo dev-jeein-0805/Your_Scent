11943f3881a38985edd92c821c69df3651ff2b0c
Jeein Sophie Han <dev.jeein.08@gmail.com>
2024-06-24 04:07:57 +0900

    feat: signup 구현 진행 중

---
+++ src/api/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_APP_FIREBASE_DB_URL,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export const join = async (email: string, password: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredential);
+  } catch (error) {
+    console.error(error);
+  }
+};
