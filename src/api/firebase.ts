07f70866399ad87ab9f9dfdf26a9ef84bcd32e53
Jeein Sophie Han <dev.jeein.08@gmail.com>
2024-06-25 00:54:33 +0900

    feat: firebase signup login logout connected

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,5 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
+import { Dispatch } from "react";
+import { AuthAction } from "../contexts/AuthContext";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
@@ -9,17 +25,112 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+const db = getFirestore(app);
+const USER_COLLECTION = collection(db, "users");
 
-export const join = async (email: string, password: string) => {
+console.log("app", app);
+
+// 회원가입
+export const join = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  if (!email || !password || !username) {
+    alert("이름과 이메일, 비밀번호를 필수로 입력해 주세요.");
+    return;
+  }
+
   try {
-    const userCredential = await createUserWithEmailAndPassword(
+    console.log("회원가입 시도 중..."); // 디버깅 로그 추가
+    const credential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-    console.log(userCredential);
+    const user = credential.user;
+    console.log("Firebase에 회원가입 성공:", user); // 디버깅 로그 추가
+
+    const userDoc = doc(USER_COLLECTION, user.uid);
+    // 해당 문서에 아래와 같은 정보를 저장한다.
+    await setDoc(userDoc, {
+      uid: user.uid,
+      email,
+      username,
+      created_at: Date.now(),
+    });
+    console.log("Firestore에 사용자 정보 저장 성공"); // 디버깅 로그 추가
+
+    // 저장이 성공하면 성공 alert를 사용자에게 보여준다.
+    alert("회원가입에 성공하셨습니다. 로그인 해주세요.");
   } catch (error) {
     console.error(error);
+    console.error("회원가입 중 오류 발생:", error); // 디버깅 로그 추가
+    const firebaseError = error as FirebaseError; // 타입 단언
+    switch (firebaseError.code) {
+      case "auth/invalid-email":
+        alert("이메일을 바르게 입력해주세요.");
+        break;
+      case "auth/weak-password":
+        alert("비밀번호가 너무 쉬워요.");
+        break;
+      case "auth/email-already-in-use":
+        alert("등록된 이메일 입니다.");
+        break;
+      default:
+        alert("회원가입 실패");
+        break;
+    }
+  }
+};
+
+// 로그인
+export const signIn = async (
+  email: string,
+  password: string,
+  dispatch: Dispatch<AuthAction>,
+  navigate: Function
+) => {
+  if (!email || !password) {
+    alert("이메일 또는 비밀번호를 모두 입력해 주세요.");
+    return;
+  }
+
+  try {
+    const { user } = await signInWithEmailAndPassword(auth, email, password);
+
+    // 유저정보 가져오기
+
+    // 'users'라는 db에 있는 user.uid에 해당되는 문서를 찾고
+    const docRef = doc(db, "users", user.uid);
+    // 찾은 문서를 가지고 온다.
+    const docSnap = await getDoc(docRef);
+    // 문서가 존재하는지 확인
+    if (docSnap.exists()) {
+      dispatch({ type: "SET_USER", payload: docSnap.data() as User });
+    } else {
+      console.log("No such document!");
+    }
+
+    // 메인페이지 이동
+    navigate("/");
+  } catch (error) {
+    alert("이메일, 비밀번호를 다시 확인해 주세요.");
+  }
+};
+
+// 로그아웃
+export const logOut = async (
+  dispatch: Dispatch<AuthAction>,
+  navigate: Function
+) => {
+  try {
+    await signOut(auth);
+    dispatch({ type: "SET_USER", payload: null });
+    localStorage.removeItem("user"); // 로컬 스토리지에서 사용자 정보 제거
+    navigate("/login");
+  } catch (error) {
+    alert("로그아웃 실패");
   }
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BsFillPencilFill } from "react-icons/bs";
 import { RiFlowerFill } from "react-icons/ri";
+import { logOut } from "../api/firebase";
+import {
+  getAuth,
+  onAuthStateChanged,
+  User as FirebaseUser,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
+import { useAuthDispatch, useAuthState } from "../contexts/AuthContext";
 
 export default function Navbar() {
+  const [user, setUser] = useState<FirebaseUser | null>(null);
+  const navigate = useNavigate();
+  const dispatch = useAuthDispatch();
+  const authState = useAuthState();
+
+  console.log(user);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      await logOut(dispatch, navigate);
+    } catch (error) {
+      console.error("Logout error: ", error);
+    }
+  };
+
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-1xl text-brand">
@@ -15,7 +51,14 @@ export default function Navbar() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        <Link to="/signup">Sign Up</Link>
+        {authState.user ? (
+          <button onClick={handleLogout}>Logout</button>
+        ) : (
+          <>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
       </nav>
     </header>
   );
